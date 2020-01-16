4ad7c442238e555f36a7fd03ee0d0693d3731f4b
Sruthi Jaganathan <sjaganat@ucsc.edu>
2020-01-15 17:47:36 -0800

    Print statement issue

--- sbi.scm
+++ sbi.scm
@@ -1,7 +1,6 @@
 #!/afs/cats.ucsc.edu/courses/cse112-wm/usr/racket/bin/mzscheme -qr
-;; always have ^^ or it won't be evaluated
-;; $Id: sbi.scm,v 1.11 2019-12-11 16:16:51-08 - - $
-;; 
+;; $Id: sbi.scm,v 1.12 2020-01-08 17:13:13-08 - - $
+;;
 ;; NAME
 ;;    sbi.scm - silly basic interpreter
 ;;
@@ -13,7 +12,6 @@
 ;;    program, which is the executed.  Currently it is only printed.
 ;;
 
-;; define the standard input/output/error
 (define *stdin* (current-input-port))
 (define *stdout* (current-output-port))
 (define *stderr* (current-error-port))
@@ -35,7 +33,6 @@
     (die `("Usage: " ,*run-file* " filename"))
 )
 
-;;file input --> linked list?
 (define (readlist-from-inputfile filename)
     (let ((inputfile (open-input-file filename)))
          (if (not (input-port? inputfile))
@@ -44,12 +41,11 @@
                   (close-input-port inputfile)
                          program))))
 
-;;the "teminal-port *stdin* = #t" part i think
 (define (dump-stdin)
     (let ((token (read)))
          (printf "token=~a~n" token)
          (when (not (eq? token eof)) (dump-stdin))))
-
+		 
 ;;holds all functions (including operators)
 ; CAN'T BE CHANGD BY USER
 (define *function-table* (make-hash))
@@ -71,7 +67,6 @@
         (^,expt)
     )
 )
-
 
 ;;holds value of all variables (updated as needed)
 ;;if variable not found, return 0
@@ -100,11 +95,53 @@
     (hash-set! *label-table* key value)
 )
 
+(define (print-statement line)
 
-;;======
-;; sbi.scm "filename"
-;;======
-;;then each line in the input ifle
+	;(printf "~s~n" (car(car(cdr line))))
+	
+	;(if (list? line)  (printf "is list~n") (printf "is not list~n"))
+	;(printf "~s~n" line)
+	;(if(eqv? (cdaar line) 'print)
+		(if (null? (cdr line))
+			(display "Nothing to print from this line ~n")
+			(when not (=(length(cdr line))0)
+				(execute-the-print (cdr line))
+			)
+		)
+		;(display "not a print ")
+	;)
+)
+
+(define (check-data-type type)
+	(cond 
+		[(string? type ) type]
+	)
+)
+
+(define (execute-the-print line)
+
+	(display "going into func1")
+	;printf "this is the whole line ~s~n" line)
+	;(printf "cdar of line is ~s~n" (cadar line))
+	(if(eqv? (caar line) 'print)
+		;(printf "recognizing its print ~n")
+		(if (string? (cdr line))
+			(display (cdr line))
+			(display (check-data-type(cdr line)))	
+		)
+	
+		;(printf "recognizing its print but in else statement ~n")
+		(when not (null?(caddr line))
+			(when (not (=(length(caddr line))0))
+
+				(execute-the-print(caddr line))
+
+			)
+		
+		)
+	)		
+)
+
 (define (write-program-by-line filename program)
     (printf "==================================================~n")
     (printf "~a: ~s~n" *run-file* filename)
@@ -112,29 +149,32 @@
     (printf "(~n")
     (for-each (lambda (line) (printf "~s~n" line)) program)
     (printf ")~n")
+	(for-each (lambda (line) (print-statement line)) program)
+	;(printf "~s~n" line))
+    (printf ")~n")
     (define program_length (length program)) ;program_length = numLines
     ;this part putting into label-table add something for the weird last line?
     (for-each 
-        (lambda (line) (set-label! (cdr line) (- (car line) 1)) )
+        (lambda (line) (set-label! (cdr line) (car line)))
         program
     )
 )
 
-;----------------------------------------------------------------
-; main function (what u do here will show up)
 (define (main arglist)
     (if (or (null? arglist) (not (null? (cdr arglist))))
         (usage-exit)
         (let* ((sbprogfile (car arglist))
                (program (readlist-from-inputfile sbprogfile)))
-              (write-program-by-line sbprogfile program)))
-
-;prints out the label-table hash to check if it works
+               (write-program-by-line sbprogfile program)
+			  
+			  ))
+			  
+			  ;prints out the label-table hash to check if it works
     (printf "*label-table*:~n")
     (hash-for-each *label-table* 
-        (lambda (key value)
-        (printf "~s = ~s~n" key value))) 
-) ;end of main
+    (lambda (key value)
+    (printf "~s = ~s~n" key value))) 			  
+);end of main
 
 (printf "terminal-port? *stdin* = ~s~n" (terminal-port? *stdin*))
 (if (terminal-port? *stdin*)

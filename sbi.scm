f6115c63332daed141bbca258e3cfa91e9e70692
Kelsy Lee <46151239+klee128@users.noreply.github.com>
2020-01-20 01:39:49 -0800

    changed print
    - prints most things 
       -yes to nested operations
       -no to arrays
    -put in function table so can do that now

--- sbi.scm
+++ sbi.scm
@@ -50,6 +50,104 @@
 ; CAN'T BE CHANGD BY USER
 (define *function-table* (make-hash))
 (define (set-function! key value) (hash-set! *function-table* key value))
+
+;;holds value of all variables (updated as needed)
+;;if variable not found, return 0
+(define *variable-table* (make-hash))
+(define (set-variable! key value)(hash-set! *variable-table* key value))
+
+
+;;holds all arrays defined in program
+;;created with make-vector and updated with vector-set!
+(define *array-table* (make-hash))
+(define (set-array! key value) (hash-set! *array-table* key value))
+
+;;holds addresses of each line
+(define *label-table* (make-hash))
+(define (set-label! key value) (hash-set! *label-table* key value))
+
+;what to do w/ input if it is a ...
+(define (parse-it word)
+    (cond 
+        ;if is string, return string
+        ((string? word) 
+            word
+        )
+        ;if number, add 0.0 to return real number
+        ((number? word) 
+            (+ word 0.0)
+        )
+        ;if pair, calculate and return the result
+        ((pair? word)
+            ;if +-*/
+            (if (hash-has-key? *function-table* (car word))
+                (cond 
+                    ;if is a procedure? if doing something and not just a vector/array
+                    ((procedure? (hash-ref *function-table* (car word)))
+                        (if (and (number? (cadr word)) (number? (caddr word)))
+                            ;if (operator number number) format
+                            ((hash-ref *function-table* (car word)) (cadr word) (caddr word))
+                            ;if nested operations?
+                            (apply (hash-ref *function-table* (car word)) (map parse-it (cdr word)))
+                        )
+                    )
+                )
+                (printf "is not in function-hash ~n")
+            )
+        )
+        ;else, output error message 
+        (else 
+            (die '("Invalid Input: " word))
+        )
+    )    
+)
+
+;goes to here from the write-program-line function
+;basically scanning to see the thing
+(define (print-statement line)          
+    ;when not just line number...
+	(when (and (not (null? (cdr line)))  (> (length (cdr line)) 0) ) 
+        ;caadr = function name (print, let, etc)
+        ;cdadr = the other stuff thats important
+        ;if found in function-table, do the thing
+        (when (hash-has-key? *function-table* (caadr line))
+            ((hash-ref *function-table* (caadr line)) (cdadr line))
+        )
+	)
+)
+ 
+;how to print :)
+;line = everything after 'print'
+(define (execute-the-print line)
+    ;base-case
+    (when (> (length line) 0)
+        (display (parse-it (car line)))    
+    )
+    ;keep going down the thingamajig until nothing else to print
+    (when (> (length line) 1 )
+        (execute-the-print (cdr line))
+    )
+    (newline)
+)
+
+(define (write-program-by-line filename program)
+    (printf "==================================================~n")
+    (printf "~a: ~s~n" *run-file* filename)
+    (printf "==================================================~n")
+    (printf "(~n")
+    (for-each (lambda (line) (printf "~s~n" line)) program)
+    (printf ")~n")
+
+    ;this part putting into label-table
+    (for-each 
+        (lambda (line)  (when (>= (length line) 2) (set-label! (cdr line) (car line)) ) )
+        program
+    )
+
+    ;go through program line-by-line and execute it
+	(for-each (lambda (line) (print-statement line)) program)
+)
+
 ;initializing *function-table*
 (for-each
     (lambda (pair) (set-function! (car pair) (cadr pair)))
@@ -64,17 +162,11 @@
         (tan, tan) (truncate, truncate)
         (+,+) (-,-) (*,*) (/,/) 
         (<,<) (>,> (<=,<=) (>=,>=))
-        (^,expt)
-        
+        (^,expt) 
+        (print, execute-the-print)      
     )
 )
 
-;;holds value of all variables (updated as needed)
-;;if variable not found, return 0
-;;table initialized w/ variables in "buildin symbols" section
-(define *variable-table* (make-hash))
-(define (set-variable! key value)
-    (hash-set! *variable-table* key value))
 ;initilizing variable-table
 (for-each
    (lambda (pair) (set-variable! (car pair) (cadr pair)))
@@ -84,89 +176,6 @@
         (pi  , (acos -1.0))
         (e   , (exp 1.0))
     )
-)
-
-;;holds all arrays defined in program
-;;created with make-vector and updated with vector-set!
-(define *array-table* (make-hash))
-
-;;holds addresses of each line
-(define *label-table* (make-hash))
-(define (set-label! key value)
-    (hash-set! *label-table* key value)
-)
-
-;goes to here from the write-program-line function
-(define (print-statement line)
-	(if (null? (cdr line))
-		(printf "Nothing to print from this line ~n")            
-        ;when not just line number...
-		(when not (=(length(cdr line))0) 
-            ;caadr = function name (print, let, etc)
-            ;cdadr = the other stuff thats important
-
-            ;if first command is print... execute the print
-            (when (eqv? (caadr line) 'print)
-                (execute-the-print (cdadr line))
-            )
-		)
-	)
-
-)
- 
-;goes to here from print-statement function
-;line = everything after 'print'
-(define (execute-the-print line)
-    ;if have something to print out
-    (when (> (length line) 0)
-        ;(printf "length of line is: ~s~n" (length line))
-        (printf "~s ~n" line)
-
-        ;if string, just print it out
-        (when (string? (car line))
-            (display (car line))
-            (newline)	
-        )
-        ;if number, add 0.0 to make real number
-        (when (number? (car line))
-            (display (+ 0.0 (car line)))
-			(newline)
-        ) 
- 
-        ;if is a pair ex. (+ 2 4) 
-        (when (pair? (car line)) 
-            (printf "is pair~n")
-            (if (hash-has-key? *function-table* (caar line))
-                (display ((hash-ref *function-table* (caar line)) (cadar line) (caddar line) ) )
-                (printf "is not in function-hash ~n")
-            )
-        )
-    )
-   
-    ;keep going down the thingamajig until nothing else to print
-    (when (> (length line) 1 )
-        (printf "in recursion~n")
-        (execute-the-print (cdr line))
-    )
-)
-
-(define (write-program-by-line filename program)
-    (printf "==================================================~n")
-    (printf "~a: ~s~n" *run-file* filename)
-    (printf "==================================================~n")
-    (printf "(~n")
-    (for-each (lambda (line) (printf "~s~n" line)) program)
-    (printf ")~n")
-
-    ;this part putting into label-table
-    ;check for syntax errors?
-    (for-each 
-        (lambda (line)  (when (>= (length line) 2) (set-label! (cdr line) (car line)) ) )
-        program
-    )
-
-    ;checking if it is a print statement
-	(for-each (lambda (line) (print-statement line)) program)
 )
 
 (define (main arglist)
